625309419fd3f54f43263e5afee8dcc4a2a6b704
Evandro Vieira <evandrovieirass@gmail.com>
2022-07-10 16:10:12 -0300

    first model-view-controller-department

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMVC.Models.ViewsModels;
 
 namespace SalesWebMVC.Controllers
 {
